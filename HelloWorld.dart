4b2e508bfdbfcb211c308fa5c87111e6c68751e7
Joshdroid250 <gutierrez25g@gmail.com>
2026-06-02 20:43:58 -0600

    firts class helloWorld

---
+++ HelloWorld.dart
@@ -0,0 +1,16 @@
+void main (){
+  
+    //var myName = 'Joshua';
+    String myName = 'Joshua';
+  
+    //final no permite cambiar el valor de una variable  
+    final String myName1 = 'Joshua';
+  
+    print('Hola $myName1');
+  
+    late final myName2 = 'pepe';
+    
+    
+    print('Hola $myName2');
+  
+}
